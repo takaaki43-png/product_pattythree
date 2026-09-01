aaf594c1c99901e7bda2e37977a74bf63ce6d4e4
太田隆章 <>
2026-09-02 00:15:45 +0900

    初回

---
+++ assets/product-media-swiper-modal.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const swiperTrigger = document.querySelectorAll('.js-swiper-modal-button');
+//   for (let trigger of swiperTrigger) {
+//     console.dir(trigger);
+//     const dataSet = trigger.parentElement.dataset;
+    
+//     if (typeof dataSet !== 'undefined') {
+//       trigger.addEventListener('click', function(e) {
+//         console.log(dataSet);
+//       });
+//     }
+//   }
+// });
